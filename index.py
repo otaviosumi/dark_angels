da09b2303e5b8c9ba0f99bf19697a8866371dbe2
kollinslima <kollins.lima@usp.br>
2018-10-28 16:49:24 -0300

    adm html page

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 import bcrypt as bc
 import cx_Oracle as cx
-from flask import Flask, render_template, request, flash, redirect, url_for, g
+from flask import Flask, render_template, request, flash, redirect, url_for, g, abort, session
 app = Flask(__name__)
 
 orcl_db = None;
@@ -32,6 +32,11 @@ def index_page():
 def login():
     return render_template('login_page.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('login'))
+
 @app.route('/validate_login', methods=['POST'])
 def validate_login():
     user_id = request.form['user_id']
@@ -45,25 +50,33 @@ def validate_login():
     orcl_db = get_db();
     cursor = orcl_db.cursor()
 
-    cursor.execute('SELECT senha, grupo FROM funcionario WHERE nregistro = ' + user_id)
+    cursor.execute('SELECT nome, senha, grupo FROM funcionario WHERE nregistro = ' + user_id)
     query = cursor.fetchall()
 
-    query_hash = query[0][0]
-    query_group = query[0][1]
+    query_user = query[0][0]
+    query_hash = query[0][1]
+    query_group = query[0][2]
 
     #Check password
     if not validate_password(password, query_hash.replace('\n','')):
         flash("ID or password is incorrect. Try again.");
         return redirect(url_for('login'))
-    
+   
+    session['username'] = query_user;
+
     if query_group == 'ADM':
-        return redirect(url_for('adm_section'));
+        return redirect(url_for('adm_section', user=query_user));
     else:
         return redirect(url_for('login'));
 
-@app.route('/adm_section')
-def adm_section():
-    return "Hello ADM"
+@app.route('/adm_section/<user>')
+def adm_section(user):
+
+    #Check if someone just type the url manually
+    if str(request.referrer).find('login') == -1:
+        abort(403)
+
+    return render_template('adm_page.html', name=user)
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12);
