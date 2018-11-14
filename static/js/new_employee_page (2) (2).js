94e4ee4e58e9fd3ac6ceaee3e91c779316cc16f5
kollinslima <kollins.lima@usp.br>
2018-11-14 13:50:24 -0200

    merge adm_view

--- static/js/new_employee_page (2) (2).js
+++ static/js/new_employee_page (2) (2).js
@@ -17,27 +17,3 @@ function changeFunc() {
 		document.getElementById("man_group").style.display = "none";
    }
 }
-
-function handleChangeEletric(checkbox) {
-        document.getElementById("man_ele_nr").style.display = "block";
-    if(checkbox.checked == true){
-    }else{
-        document.getElementById("man_ele_nr").style.display = "none";
-   }
-}
-
-function handleChangeMecanic(checkbox) {
-    if(checkbox.checked == true){
-        document.getElementById("man_mec_nr").style.display = "block";
-    }else{
-        document.getElementById("man_mec_nr").style.display = "none";
-   }
-}
-
-function handleChangeTI(checkbox) {
-    if(checkbox.checked == true){
-        document.getElementById("man_ti_nr").style.display = "block";
-    }else{
-        document.getElementById("man_ti_nr").style.display = "none";
-   }
-}
